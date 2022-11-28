e79e4ce390ba8c6384931d7757de0f85b6982726
Martin Paniagua <martin.paniagua200013@gmail.com>
2022-11-28 00:54:36 -0600

    se agrega toda la logica de las abejitas

--- src/scenes/NivelA4.js
+++ src/scenes/NivelA4.js
@@ -15,6 +15,7 @@ class  NivelA4 extends Phaser.Scene {
     }
 
     create() {
+        this.tocoPanal = true
         this.physics.world.setBounds(0,0,1580, 700);
         //CAMARA INICIAL EFECTO FADE IN
         this.cameras.main.setBounds(0, 0, 1580, 780);
@@ -22,111 +23,10 @@ class  NivelA4 extends Phaser.Scene {
         //Imagen de Fondo
         this.fondo = this.add.image(775, 360, 'NivelA4/NivelA4').setDepth(-2).setScale(.4,.35);
         this.mesa = this.physics.add.staticImage(200, 630, 'NivelA4/mesa').setScale(2);
-        this.mesa.body.setSize(180,15);
+        this.mesa.body.setSize(140,15);
         this.mesa.setOffset(-0.20,-10);
         this.panal = this.physics.add.image(1270,515, 'NivelA4/panal').setScale(1.2);
         this.panal.body.setAllowGravity(false);
-        // this.abeja = this.add.image(1270,515, 'NivelA4/abeja').setScale(0.2);
-        this.abejas = this.physics.add.group({
-            key: 'abeja',
-            repeat: 2,
-            setXY: {
-              x: 240,
-              y: 680,
-              stepY: -30,
-              stepX: 50
-            }
-          });
-        this.abejas.children.iterate((abejas) => {
-            abejas.setScale(0.18);
-            abejas.body.setAllowGravity(false);
-            abejas.setCollideWorldBounds(true);
-        });
-        this.add.tween({
-            targets: this.abejas.getChildren(),
-            x: 1500,
-            yoyo: true,
-            duration: 4000,
-            repeat: -1,
-            easy: 'Power1',
-            onYoyo: () => {
-              this.abejas.children.iterate((abeja) => {
-                abeja.flipX = 1;
-              });
-            },
-            onRepeat: () => {
-              this.abejas.children.iterate((abeja) => {
-                abeja.flipX = 0;
-              });
-            }
-          });
-        this.abejas2 = this.physics.add.group({
-            key: 'abeja',
-            repeat: 1,
-            setXY: {
-                x: 1500,
-                y: 650,
-                stepY: -30,
-                stepX: 50
-            }
-        });
-        this.abejas2.children.iterate((abejas) => {
-            abejas.setScale(0.18);
-            abejas.body.setAllowGravity(false);
-            abejas.setCollideWorldBounds(true);
-            abejas.flipX = 1;
-        });
-        this.add.tween({
-            targets: this.abejas2.getChildren(),
-            x: 240,
-            yoyo: true,
-            duration: 4000,
-            repeat: -1,
-            easy: 'Power1',
-            onYoyo: () => {
-              this.abejas2.children.iterate((abeja) => {
-                abeja.flipX = 0;
-              });
-            },
-            onRepeat: () => {
-              this.abejas2.children.iterate((abeja) => {
-                abeja.flipX = 1;
-              });
-            }
-          });
-        this.abejas3 = this.physics.add.group({
-            key: 'abeja',
-            repeat: 3,
-            setXY: {
-                x: 350,
-                y: 550,
-                stepY: -30,
-                stepX: -30
-            }
-        });
-        this.abejas3.children.iterate((abejas) => {
-            abejas.setScale(0.18);
-            abejas.body.setAllowGravity(false);
-            abejas.setCollideWorldBounds(true);
-        });
-        this.add.tween({
-            targets: this.abejas3.getChildren(),
-            x: 1500,
-            yoyo: true,
-            duration: 6000,
-            repeat: -1,
-            easy: 'Power1',
-            onYoyo: () => {
-              this.abejas3.children.iterate((abeja) => {
-                abeja.flipX = 1;
-              });
-            },
-            onRepeat: () => {
-              this.abejas3.children.iterate((abeja) => {
-                abeja.flipX = 0;
-              });
-            }
-          });
         //PERSONAJES
         //Perro Javier
         this.dog = this.physics.add.sprite(100, 610, 'Dog', 0).setScale(0.2);
@@ -148,6 +48,14 @@ class  NivelA4 extends Phaser.Scene {
         this.physics.add.collider(this.dog, this.mesa, () => {});
         //Teclado
         this.cursors = this.input.keyboard.createCursorKeys();
+
+
+        //Colision con gatita
+        this.physics.add.collider(this.dog, this.gata, () => {
+            this.dog.setVelocityY(0);
+            this.dog.setAccelerationY(0);
+            this.scene.start('NivelA5');
+        });
     }
 
     update(time, delta) {
@@ -157,40 +65,151 @@ class  NivelA4 extends Phaser.Scene {
         {
             this.dog.anims.play('dogIdle',true);
         }
-        if (this.cursors.left.isDown)
-        {
-            this.dog.setVelocityX(-200);
-            this.dog.anims.play('dogC',true);
-            this.dog.flipX=1;
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.dog.setVelocityX(200);
-            this.dog.anims.play('dogC',true);
-            this.dog.flipX=0;
-        }
-        else
-        {
-            this.dog.setVelocityX(0);
-        }
-
-        if ((this.cursors.up.isDown && this.dog.body.onFloor()))
-        {
-            this.dog.setVelocityY(-500);
-        }
-
-        if(this.dog.x > 1260) {
-            this.cameras.main.shake(500,0.008);
+        
+        if(this.gata.x > 1280) {
+            // se le permite al perro avanzar 
+            if (this.cursors.left.isDown)
+            {
+                this.dog.setVelocityX(-200);
+                this.dog.anims.play('dogC',true);
+                this.dog.flipX=1;
+            }
+            else if (this.cursors.right.isDown)
+            {
+                this.dog.setVelocityX(200);
+                this.dog.anims.play('dogC',true);
+                this.dog.flipX=0;
+            }
+            else
+            {
+                this.dog.setVelocityX(0);
+            }
+    
+            if ((this.cursors.up.isDown && this.dog.body.onFloor()))
+            {
+                this.dog.setVelocityY(-500);
+            }
+            // se cae panal y se crean los grupos de las abejas
+            this.cameras.main.shake(500,0.005);
             this.panal.body.setAllowGravity(true);
             this.panal.angle = 30
+            if(this.tocoPanal == true) {
+                this.abejas = this.physics.add.group({
+                    key: 'abeja',
+                    repeat: 2,
+                    setXY: {
+                      x: 240,
+                      y: 680,
+                      stepY: -30,
+                      stepX: 50
+                    }
+                  });
+                this.abejas.children.iterate((abejas) => {
+                    abejas.setScale(0.18);
+                    abejas.body.setAllowGravity(false);
+                    abejas.setCollideWorldBounds(true);
+                });
+                this.add.tween({
+                    targets: this.abejas.getChildren(),
+                    x: 1500,
+                    yoyo: true,
+                    duration: 4000,
+                    repeat: -1,
+                    easy: 'Power1',
+                    onYoyo: () => {
+                      this.abejas.children.iterate((abeja) => {
+                        abeja.flipX = 1;
+                      });
+                    },
+                    onRepeat: () => {
+                      this.abejas.children.iterate((abeja) => {
+                        abeja.flipX = 0;
+                      });
+                    }
+                  });
+                this.abejas2 = this.physics.add.group({
+                    key: 'abeja',
+                    repeat: 1,
+                    setXY: {
+                        x: 1500,
+                        y: 650,
+                        stepY: -30,
+                        stepX: 50
+                    }
+                });
+                this.abejas2.children.iterate((abejas) => {
+                    abejas.setScale(0.18);
+                    abejas.body.setAllowGravity(false);
+                    abejas.setCollideWorldBounds(true);
+                    abejas.flipX = 1;
+                });
+                this.add.tween({
+                    targets: this.abejas2.getChildren(),
+                    x: 240,
+                    yoyo: true,
+                    duration: 4000,
+                    repeat: -1,
+                    easy: 'Power1',
+                    onYoyo: () => {
+                      this.abejas2.children.iterate((abeja) => {
+                        abeja.flipX = 0;
+                      });
+                    },
+                    onRepeat: () => {
+                      this.abejas2.children.iterate((abeja) => {
+                        abeja.flipX = 1;
+                      });
+                    }
+                  });
+                this.abejas3 = this.physics.add.group({
+                    key: 'abeja',
+                    repeat: 3,
+                    setXY: {
+                        x: 350,
+                        y: 550,
+                        stepY: -30,
+                        stepX: -30
+                    }
+                });
+                this.abejas3.children.iterate((abejas) => {
+                    abejas.setScale(0.18);
+                    abejas.body.setAllowGravity(false);
+                    abejas.setCollideWorldBounds(true);
+                });
+                this.add.tween({
+                    targets: this.abejas3.getChildren(),
+                    x: 1500,
+                    yoyo: true,
+                    duration: 6000,
+                    repeat: -1,
+                    easy: 'Power1',
+                    onYoyo: () => {
+                      this.abejas3.children.iterate((abeja) => {
+                        abeja.flipX = 1;
+                      });
+                    },
+                    onRepeat: () => {
+                      this.abejas3.children.iterate((abeja) => {
+                        abeja.flipX = 0;
+                      });
+                    }
+                  });
+                  this.tocoPanal = false;
+            }
+            
+        } else {
+            this.gata.x += 1;
         }
-        function choqueAbeja(javier, abeja) {
-            javier.setTint(0xff0000);
-            // bandera+=1;
-            // console.log(bandera);
-            // quitarCora();
-          }
+
+        // Logica de colisiones con abejas
+        let choqueAbeja = (javier, abeja) => {
+            this.scene.restart()
+        }
         this.physics.collide(this.dog, this.abejas, choqueAbeja);
+        this.physics.collide(this.dog, this.abejas2, choqueAbeja);
+        this.physics.collide(this.dog, this.abejas3, choqueAbeja);
+
+
     }
 }
 

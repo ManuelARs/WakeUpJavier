ff3458f17288306516bb27892ad3d03a4df676ff
ManuelARs <m.a.r.s.232001@gmail.com>
2022-11-27 22:02:16 -0600

    Funcion de setBounds para limites

--- src/scenes/NivelA1.js
+++ src/scenes/NivelA1.js
@@ -13,7 +13,7 @@ class NivelA1 extends Phaser.Scene{
     create() {
         // this.scene.moveAbove('Bootloader','HUD');
         //console.log(this.scene.manager.scenes);
-        
+        this.physics.world.setBounds(0,0,1580, 720)
         //CAMARA INICIAL EFECTO FADE IN
         this.cameras.main.setBounds(0, 0, 1580, 780);
         this.cameras.main.fadeIn(1000);
@@ -24,10 +24,7 @@ class NivelA1 extends Phaser.Scene{
         // this.musicaFondo.play();
         //FONDO Y SPRITE
         this.fondo = this.add.image(800, 395, 'NivelA1/NivelA1').setDepth(-2).setScale(.37,.35);
-        this.pasto = this.physics.add.image(790, 740, 'NivelA1/pasto').setDepth(-1).setScale(.42,.27);
-        this.pasto.body.setAllowGravity(false);
-        this.pasto.body.setImmovable(true);
-        this.pasto.body.setSize(3800,250);
+
         //PERSONAJES
         //Perro Javier
         this.dog = this.physics.add.sprite(100, 610, 'Dog', 0).setScale(0.2);
@@ -55,8 +52,6 @@ class NivelA1 extends Phaser.Scene{
         //COLISIONES
         this.dog.body.setCollideWorldBounds(true);
         this.gata.body.setCollideWorldBounds(true);
-
-        //this.physics.add.existing(this.gata, true );
   
         this.physics.add.collider(this.dog, this.gata, () => {
             // this.tweens = this.add.tween({
@@ -78,9 +73,7 @@ class NivelA1 extends Phaser.Scene{
             this.dog.setAccelerationY(0);
             this.scene.start('NivelA2');
         });
-        //COLISIÓN PASTO CON PERRO Y GATA
-        this.physics.add.collider(this.dog, this.pasto, () => {});
-        this.physics.add.collider(this.gata, this.pasto, () => {});
+
 
         //Teclado
         this.cursors = this.input.keyboard.createCursorKeys();

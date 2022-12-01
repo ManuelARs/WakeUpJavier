16b1fcd5d98d3f2fd597b74743210d474a879999
AMG2592 <almorga2592@gmail.com>
2022-11-30 21:41:12 -0600

    Se cambia la escena inicial en el Bootloader

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -102,7 +102,7 @@ class Bootloader extends Phaser.Scene{
     }
 
     create(){
-        this.scene.start('NivelB1');
+        this.scene.start('NivelC4');
         //this.scene.launch('Menu');
         //console.log(this.scene.manager.scenes)
     }
